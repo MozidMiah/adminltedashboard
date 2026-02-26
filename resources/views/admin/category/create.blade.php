e7a20782d3b170d236c603f48b60734ae1281cff
MozidMiah <mazidkhan278@gmail.com>
2026-02-27 03:05:24 +0600

    new dashboard add

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -11,8 +11,6 @@
                     <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
                     <li class="breadcrumb-item active">Category</li>
                 </ol>
-                {{-- <button type="button" class="btn btn-info d-none d-lg-block m-l-15 text-white"><i
-                        class="fa fa-plus-circle"></i> Create New</button> --}}
             </div>
         </div>
     </div>
